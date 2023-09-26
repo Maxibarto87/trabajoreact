d171959f106c3b026f10f87a7038ababfa01e98c
Maxibarto87 <barto_02_07_87@outlook.com>
2023-09-25 23:48:53 -0300

    first commit

---
+++ button.js
@@ -0,0 +1,23 @@
+
+// ¡Esto es un componente React!
+
+const Button = () =>
+
+    React.CreateElement(
+    "button",
+    { onClick: () => alert("Has hecho click!") },
+    "Haz Click!");
+
+// Seleccionamos el div dentro de nuestro index.html
+
+const domContainer = document.getElementById("button_container");
+
+// Aquí llamamos a un método de React que crea la base
+
+// para poder utilizar la librería dentro de nuestro <div>.
+
+const root = ReactDOM.createRoot(domContainer);
+
+// Con el método render vamos a mostrar el componente Button
+
+root.render(React.CreateElement(button));
